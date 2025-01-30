e851c6fce0e2737d57552dcddd0b449ae3b81427
Melle <93864179+mellever@users.noreply.github.com>
2025-01-30 14:55:15 +0100

    Delete testCUDA.py

--- testCUDA.py
+++
@@ -1,6 +0,0 @@
-#script to ensure cuda is working
-
-import torch
-print(torch.cuda.is_available())  # Should return True
-print(torch.version.cuda)  # Prints the CUDA version
-print(torch.backends.cudnn.version())  # Prints cuDNN version
